ada821be7c8d2759710c3a5d59b90d9766e21272
anthonycornner <cornneranthony@gmail.com>
2023-04-06 23:36:40 -0400

    Create Hello World
    Figuring out GitHub desktop

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start ()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
