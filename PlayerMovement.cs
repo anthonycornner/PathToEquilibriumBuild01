0b8d1e83a1a5d7a12d0b126f43180f357acf08ec
anthonycornner <cornneranthony@gmail.com>
2023-04-07 00:21:27 -0400

    Add player movement (horizontal)

---
+++ PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// give player object physics to move left and right, jump will be in a different script
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    // necessary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    // variables to play with
+    public float speed = 2.0f;
+    public float horizMovement; // = 1 or -1 or 0
+
+    private void Start()
+    {
+        // define the game objects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // handles the input for physics
+    private void Update()
+    {
+        // check direction given by player
+        horizMovement = Input.GetAxisRaw("Horizontal");
+    }  
+
+    // handles running the physics
+    private void FixedUpdate() 
+    {
+        // move the character left and right
+        rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+    }
+}
