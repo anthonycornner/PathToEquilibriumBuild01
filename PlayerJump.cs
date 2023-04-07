2079e1d8b593b2d6a4eeacc97a3018aa48724006
anthonycornner <cornneranthony@gmail.com>
2023-04-07 02:21:35 -0400

    Refactor and make code more readable
    Add code readability and functionality to "mario jump" i.e. give the jump mechanic a weight to be able to jump more precisely

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -6,23 +6,69 @@ public class PlayerJump : MonoBehaviour
 {
     // force, apply force, 1x, sends player moving upwards gravity moves player back down
     //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
+    [Header("Jump Details")]
     public float jumpForce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
+
+    [Header("Ground Details")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float radOCircle;
+    [SerializeField] private LayerMask whatIsGround;
     public bool isGrounded;
+
+    [Header("Components")]
     private Rigidbody2D rb;
+
+
+    
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
     }
 
     private void Update()
     {
-        isGrounded = Physics2D.OverlapCircle();
-        //use space and w keys to jump respectively
-        if (Input.GetButtonDown("Jump"))
+        // what it means to be grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,radOCircle, whatIsGround);
+
+        if(isGrounded )
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter = jumpTime;
         }
+
+        //if we press the space and w keys to jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //jump
+            stoppedJumping = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            
+        }
+
+        // if we hold the jump button
+        if (Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0))
+        {
+            //keep applying the same force to the jump
+            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        //if we release the jump button
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, radOCircle);
     }
 }
